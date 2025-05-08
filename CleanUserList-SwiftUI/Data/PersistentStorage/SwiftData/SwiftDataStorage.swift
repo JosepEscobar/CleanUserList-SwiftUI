b49d9b8fb1b61c782f0b0435f325b0353088ff02
Josep Escobar <josepescobarn@gmail.com>
2025-05-08 22:39:27 +0200

    fix errors with predicates

--- CleanUserList-SwiftUI/Data/PersistentStorage/SwiftData/SwiftDataStorage.swift
+++ CleanUserList-SwiftUI/Data/PersistentStorage/SwiftData/SwiftDataStorage.swift
@@ -2,98 +2,6 @@ import Foundation
 import SwiftData
 import Combine
 
-@Model
-final class UserEntity {
-    @Attribute(.unique) var id: String
-    var name: String
-    var surname: String
-    var fullName: String
-    var email: String
-    var phone: String
-    var gender: String
-    var street: String
-    var city: String 
-    var state: String
-    var registeredDate: Date
-    var largeImageURL: String
-    var mediumImageURL: String
-    var thumbnailImageURL: String
-    
-    init(
-        id: String,
-        name: String,
-        surname: String,
-        fullName: String,
-        email: String,
-        phone: String,
-        gender: String,
-        street: String,
-        city: String,
-        state: String,
-        registeredDate: Date,
-        largeImageURL: String,
-        mediumImageURL: String,
-        thumbnailImageURL: String
-    ) {
-        self.id = id
-        self.name = name
-        self.surname = surname
-        self.fullName = fullName
-        self.email = email
-        self.phone = phone
-        self.gender = gender
-        self.street = street
-        self.city = city
-        self.state = state
-        self.registeredDate = registeredDate
-        self.largeImageURL = largeImageURL
-        self.mediumImageURL = mediumImageURL
-        self.thumbnailImageURL = thumbnailImageURL
-    }
-    
-    func toDomain() -> User {
-        return User(
-            id: id,
-            name: name,
-            surname: surname,
-            fullName: fullName,
-            email: email,
-            phone: phone,
-            gender: gender,
-            location: Location(
-                street: street,
-                city: city,
-                state: state
-            ),
-            registeredDate: registeredDate,
-            picture: Picture(
-                large: URL(string: largeImageURL)!,
-                medium: URL(string: mediumImageURL)!,
-                thumbnail: URL(string: thumbnailImageURL)!
-            )
-        )
-    }
-    
-    static func fromDomain(user: User) -> UserEntity {
-        return UserEntity(
-            id: user.id,
-            name: user.name,
-            surname: user.surname,
-            fullName: user.fullName,
-            email: user.email,
-            phone: user.phone,
-            gender: user.gender,
-            street: user.location.street,
-            city: user.location.city,
-            state: user.location.state,
-            registeredDate: user.registeredDate,
-            largeImageURL: user.picture.large.absoluteString,
-            mediumImageURL: user.picture.medium.absoluteString,
-            thumbnailImageURL: user.picture.thumbnail.absoluteString
-        )
-    }
-}
-
 class SwiftDataStorage: UserStorage {
     private let modelContainer: ModelContainer
     
@@ -119,8 +27,10 @@ class SwiftDataStorage: UserStorage {
                     // Para cada entidad, verificar si ya existe y solo agregar si no existe
                     for entity in userEntities {
                         // Buscar si existe un usuario con el mismo ID
-                        let descriptor = FetchDescriptor<UserEntity>(predicate: #Predicate { $0.id == entity.id })
-                        let existingUsers = try modelContext.fetch(descriptor)
+                        let descriptor = FetchDescriptor<UserEntity>()
+                        // Filtramos manualmente ya que los predicados dan problemas
+                        let allUsers = try modelContext.fetch(descriptor)
+                        let existingUsers = allUsers.filter { $0.id == entity.id }
                         
                         // Si no existe, agregarlo
                         if existingUsers.isEmpty {
@@ -169,8 +79,9 @@ class SwiftDataStorage: UserStorage {
             Task { @MainActor in
                 do {
                     let modelContext = self.modelContainer.mainContext
-                    let descriptor = FetchDescriptor<UserEntity>(predicate: #Predicate { $0.id == id })
-                    let usersToDelete = try modelContext.fetch(descriptor)
+                    let descriptor = FetchDescriptor<UserEntity>()
+                    let allUsers = try modelContext.fetch(descriptor)
+                    let usersToDelete = allUsers.filter { $0.id == id }
                     
                     for user in usersToDelete {
                         modelContext.delete(user)
