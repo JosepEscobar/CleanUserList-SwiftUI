b253300db6e1cc34b07b28619099ed8b58f40387
Josep Escobar <josepescobarn@gmail.com>
2025-05-08 20:34:45 +0200

    Initial Commit

---
+++ CleanUserList-SwiftUI/CleanUserList_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanUserList_SwiftUIApp.swift
+//  CleanUserList-SwiftUI
+//
+//  Created by Josep Escobar on 8/5/25.
+//
+
+import SwiftUI
+
+@main
+struct CleanUserList_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CleanUserList-SwiftUITests/CleanUserList_SwiftUITests.swift
@@ -0,0 +1,16 @@
+//
+//  CleanUserList_SwiftUITests.swift
+//  CleanUserList-SwiftUITests
+//
+//  Created by Josep Escobar on 8/5/25.
+//
+
+import Testing
+
+struct CleanUserList_SwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
